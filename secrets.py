1f16e6b001ed3a4333a8a08a5a9f4aecc6b5ee06
mahananaka <jpaeschk@uoregon.edu>
2016-10-28 19:45:38 -0700

    added comments

--- secrets.py
+++ secrets.py
@@ -1,2 +1,6 @@
 pk.eyJ1IjoibWFoYW5hbmFrYSIsImEiOiJjaXVydzZkNnMwMDl3Mnpxamc2YnJqdTN6In0.QN32luonRpWSlq5zXruTYA
 mahananaka.1p03d9fb
+#this is the public access key and the project id, while this are visible to the public
+#I decided to place them here and retrieve these values from here so that if a reason
+#arises where the access key needs changed I can change one location and it affect the 
+#whole project
